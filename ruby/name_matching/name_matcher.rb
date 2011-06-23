f297129abb115d38782185533ac143dc37315326
Alan Feuerlein <alan@observepoint.com>
2011-06-23 11:37:07 -0600

    removed the license, seems odd for now

--- ruby/name_matching/name_matcher.rb
+++ ruby/name_matching/name_matcher.rb
@@ -1,21 +1,4 @@
 #NAME-MATCHING v.1, attempt to identify similar and equivalent names in a list of them
-#############################
-#Copyright (C) 2011 Alan Feuerlein
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#############################
 #This script does several things,
 
 #1 - reads data in from a file that is formatted as "string","string1","string2","...etc and sanitizes the data
